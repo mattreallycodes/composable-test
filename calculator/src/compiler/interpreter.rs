99e598f4ad195cc2ea1d6d0c092b7371d6538668
SA729i1234sdjhsa <mattreallycodes@protonmail.com>
2022-06-07 10:59:51 -0400

    interpreter tests working successfully.

--- calculator/src/compiler/interpreter.rs
+++ calculator/src/compiler/interpreter.rs
@@ -60,12 +60,12 @@ mod tests {
     fn basics() {
         assert_eq!(Interpreter::from_source("1 * 2").unwrap() as i32, 2);
         assert_eq!(Interpreter::from_source("1 + 2").unwrap() as i32, 3);
-        // assert_eq!(Interpreter::source("(1 + 2)").unwrap() as i32, 3);
-        // assert_eq!(Interpreter::from_source("2 + (2 - 1)").unwrap() as i32, 3);
-        // assert_eq!(Interpreter::from_source("(2 + 3) - 1").unwrap() as i32, 4);
-        //  assert_eq!(
-        //    Interpreter::from_source("1 + ((2 + 3) - (2 + 3))").unwrap() as i32,
-        //    1
-        // );
+     // assert_eq!(Interpreter::source("(1 + 2)").unwrap() as i32, 3);
+        assert_eq!(Interpreter::from_source("2 + (2 - 1)").unwrap() as i32, 3);
+        assert_eq!(Interpreter::from_source("(2 + 3) - 1").unwrap() as i32, 4);
+        assert_eq!(
+            Interpreter::from_source("1 + ((2 + 3) - (2 + 3))").unwrap() as i32,
+            1
+        );
     }
 }
